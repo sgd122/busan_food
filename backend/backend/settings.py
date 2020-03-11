f258957b1664196917089221bec9f628ef84ae3f
[도울] 성기동 <dirrksdl@naver.com>
2020-03-11 17:17:17 +0900

    backend create

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(_g%!t*@df4&y_t-2bsrjwaa0lz@4=vvkb*ech86-rswqjtcqa'
+SECRET_KEY = 'i-rqqd4%a35*!d=yhu*)b6)2^mrrn5@9-&m-i2o+xg4pam8jk)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
