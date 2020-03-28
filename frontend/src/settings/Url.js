938cb31d788a4a771057e2b208bed9924aaebafe
[도울] 성기동 <dirrksdl@naver.com>
2020-03-28 21:17:39 +0900

    Update Url.js

--- frontend/src/settings/Url.js
+++ frontend/src/settings/Url.js
@@ -1,1 +1,12 @@
-export const BASE_URL = "http://localhost:8001/";
+const homePort = "8000";
+let dev_url = "http://13.209.3.103:8001/";
+let url = ""
+if (
+    window.location.href.indexOf("localhost") > -1 ||
+    window.location.href.indexOf("127.0.0.1") > -1
+) {
+    url = "http://localhost:8001/";
+} else {
+    url = dev_url;
+}
+export const BASE_URL = url;
